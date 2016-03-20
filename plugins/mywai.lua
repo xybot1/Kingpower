dee5f3615e7ebf79ec9a981eee89d56432ae9059
mohammad12344 <sdf12345641@gmail.com>
2016-03-21 02:41:37 +0430

    Update mywai.lua

--- plugins/mywai.lua
+++ plugins/mywai.lua
@@ -1,30 +1,29 @@
 do
 
 local function run(msg, matches)
-  if matches[1] == 'me' then
+  if matches[1] == 'من' then
     if is_sudo(msg) then
     send_document(get_receiver(msg), "/root/robot/sudo.webp", ok_cb, false)
-      return "You are My SUDO"
+      return "تو سودومی"
     elseif is_admin(msg) then
     send_document(get_receiver(msg), "/root/robot/admin.webp", ok_cb, false)
-      return "You are My ADMIN"
+      return "تو ادمینمی"
     elseif is_owner(msg) then
     send_document(get_receiver(msg), "/root/robot/owner.webp", ok_cb, false)
-      return "You are Group Owner"
+      return "تو ادمین اصلی گروهی"
     elseif is_momod(msg) then
     send_document(get_receiver(msg), "/root/robot/mod.webp", ok_cb, false)
-      return "You are Group Mod"
+      return "تو مدیر گروهی"
     else
     send_document(get_receiver(msg), "/root/robot/mmbr.webp", ok_cb, false)
-      return "You are Group Member"
+      return "تو یک کاربر معمولی در گروه هستی"
     end
   end
 end
 
 return {
   patterns = {
-    "^[!/]([Mm]e)$",
-    "^([Mm]e)$"
+    "^(من)$"
     },
   run = run
 }
